fcc772fc85f4f08450723e3abe4e040e0237f0db
BazylevDV <dmbazylev@gmail.com>
2024-07-09 16:46:16 +0500

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,23 @@
+students_ = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}# дан школьный журнал с именами
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]# даны оценки за месяц
+print("Выведем показатели учеников: ")
+Jo=students_.pop('Johnny')
+print( J)
+print (("Вот  его оценки за четверть:"), grades[0], ", средняя оценка за четверть:")
+J=grades [0][0]=(5+3+3+5+4)/5#считаем среднюю оценку 1 элемента в списке учеников
+print(J)
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
